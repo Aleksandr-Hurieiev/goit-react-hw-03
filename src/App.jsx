4a9100e495fc56118c35964377bd677731ac2f87
Aleksander <hurieiev.al@gmail.com>
2024-08-04 20:15:39 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,33 @@ import ContactForm from "./components/ContactForm/ContactForm.jsx";
 import SearchBox from "./components/SearchBox/SearchBox.jsx";
 import ContactList from "./components/ContactList/ContactList.jsx";
 import { useState } from "react";
-//
+import { useEffect } from "react";
+
 function App() {
-  const [usersContact, setUser] = useState(userListJson);
-  //
+  // получение данный с localStorage + проверка
+  const [usersContact, setUser] = useState(() => {
+    const localUsers = localStorage.getItem("contsctsUsers");
+    if (localUsers) {
+      return JSON.parse(localUsers);
+    }
+    return usersContact;
+  });
   const [filter, setFilter] = useState("");
-  //
-  const handelClick = (event) => {
-    const value = event.target.value
-    console.log(value);
+  // сохранение в localStorage
+  useEffect(() => {
+    localStorage.setItem("contsctsUsers", JSON.stringify(usersContact));
+  }, [usersContact]);
+
+  // добавление обьекта ..
+  const onNewUser = (newFormUser) => {
+    const finalUser = {
+      ...newFormUser,
+      id: nanoid(),
+    };
+    console.log(finalUser);
+    setUser([finalUser, ...usersContact]);
   };
-  //
-
-  const filterUsers = usersContact.filter((user) =>
-    user.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  console.log(filterUsers);
-  //
+  // удаление обьекта ..
   const onDelite = (userId) => {
     setUser((prefUser) => {
       return prefUser.filter((user) => {
@@ -31,14 +40,17 @@ function App() {
       });
     });
   };
+  // фильтрация обьекта ..
+  const filterUsers = usersContact.filter((user) =>
+    user.name.toLowerCase().includes(filter.toLowerCase())
+  );
   //
   return (
     <div>
       <h1>Phonebook</h1>
-      <p>{filter}</p>
-      <ContactForm />
+      <ContactForm onNewUser={onNewUser} />
       <SearchBox value={filter} onFilter={setFilter} />
-      <ContactList userList={usersContact} onDelite={onDelite} />
+      <ContactList userList={filterUsers} onDelite={onDelite} />
     </div>
   );
 }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,65 @@
-const ContactForm = () => {
+import css from "../ContactForm/ContactForm.module.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { nanoid } from "nanoid";
+const ContactForm = ({ onNewUser }) => {
+  const initialValue = {
+    firstName: "",
+    number: "",
+  };
+  const firstNameId = nanoid();
+  const numberId = nanoid();
+  const handleSubmit = (values, actions) => {
+    const newUser = {
+      name: values.firstName,
+      number: values.number,
+    };
+    onNewUser(newUser);
+    actions.resetForm();
+  };
+  const FeedbackSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("please enter your name"),
+    number: Yup.string()
+      .matches((/^\+380\d{3}\d{2}\d{2}\d{2}$/), "valid phone number in the format +38 ххх ххх хх хх")
+      .required("please enter your phone number in the format +380 хх ххх хх хх"),
+  });
   return (
-    <div>
-     ghdtn 
-    </div>
+    <Formik
+      initialValues={initialValue}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
+      <Form className={css.form}>
+        <label htmlFor={firstNameId}>First Name</label>
+        <Field
+          id={firstNameId}
+          type="text"
+          name="firstName"
+          placeholder="Name"
+        />
+        <ErrorMessage
+          name="firstName"
+          component="span"
+          className={css.formError}
+        />
+        <label htmlFor={numberId}>Number</label>
+        <Field
+          id={numberId}
+          type="tel"
+          name="number"
+          placeholder="Number tell"
+        />
+        <ErrorMessage
+          name="number"
+          component="span"
+          className={css.formError}
+        />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 
